2e7032f239e642d89a84fae33d1138489684d4d9
Soukou-boop <olugsoukou@gmail.com>
2025-03-17 00:28:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function showDiv() {
 function hideDiv() {
     document.getElementById("contactpage").style.display = "none";
 }
+
+//button contact whatsapp
+function contactSeller() {
+    let phoneNumber = "+2250173659493"; // Replace with the actual phone number
+    let message = encodeURIComponent("Salut monsieur Soukou! nous venons de visiter votre portfolio et aimerions nous entretenir avec vous pour en savoir plus, merci!");
+    let url = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.location.href = url;
+}
+
+//for email
+function sendEmail() {
+    // Replace with your email address
+    let email = "eskadebelair@gmail.com";
+    let subject = "Un entretien";
+    let body = "Salut monsieur Soukou Franck-Axel! nous venons de visiter votre portfolio et aimerions nous entretenir avec vous pour en savoir plus, merci!";
+
+    // Format the mailto link
+    let mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    // Open email client
+    window.location.href = mailtoLink;
+}
